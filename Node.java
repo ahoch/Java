831e1e1c03752a2baf542b0a4706c514b96b17f1
ahoch <ahochstetler@gmail.com>
2019-08-19 17:44:43 -0400

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Node<T extends Comparable<T>> implements Comparable<Node<T>> {
+	
+		private T contents;
+		private Node<T> next = null;
+	
+		Node(T c) {
+			contents = c;
+		}
+	
+		private void setNext(Node n) {
+			next = n;
+		}
+	
+		private Node getNext() {
+			return next;
+		}
+		
+		private T getContents() {
+			return contents;
+		}
+		
+		@Override
+		public int compareTo(Node<T> n) {
+			return getContents().compareTo(n.getContents());
+		}
+}
